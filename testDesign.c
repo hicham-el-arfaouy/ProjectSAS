d7c08a08d06d6e6ce7e62b2615a91f6afa1b5ab5
hicham <hicham.elarfaouy.youcode@gmail.com>
2022-09-19 17:30:14 +0100

    fichier v1

--- testDesign.c
+++ testDesign.c
@@ -25,106 +25,27 @@ struct Medicine medicineArray[100] = {
         {2, "12345678901", 0, 35.99},
     };
 
-void toUpperCase(char string[20]){
-    for(int i = 0; i < strlen(string); i++){
-        string[i] = toupper(string[i]);
-    }
-}
-
 void main(){
+    // FILE* fichier = NULL;
 
-
-    char choice[20];
-    int choicee;
-    
-    do{
-        printf("\t -------------------------------------------------------\t\n");
-        printf("\t|\t\t\tMENU\t\t\t\t|\n");
-        printf("\t -------------------------------------------------------\t\n");
-        printf("\t|\t{ 1 }\t:\tShow Medicines\t\t\t|\n");
-        printf("\t|\t{ 2 }\t:\tAdd Medicines\t\t\t|\n");
-        printf("\t|\t{ 3 }\t:\tUpdate a Medicine\t\t|\n");
-        printf("\t|\t{ 4 }\t:\tSearch for a Medicine\t\t|\n");
-        printf("\t|\t{ 5 }\t:\tDelete a Medicine\t\t|\n");
-        printf("\t|\t{ 6 }\t:\tBuy a Medicine\t\t\t|\n");
-        printf("\t|\t{ 7 }\t:\tDashboard\t\t\t|\n");
-        printf("\t|\t{ 0 }\t:\tExit\t\t\t\t|\n");
-        printf("\t -------------------------------------------------------\t\n");
-        printf("Please enter your choice : ");
-        scanf("%s", &choice);
-        choicee = atoi(choice);
-        printf("Please enter your choice : %d", choicee);
-        switch(choicee){
-            case 0: printf("0"); break;
-            case 1: printf("0"); break;
-            case 2: printf("0");  break;
-            case 3: printf("0");  break;
-            case 4: printf("0");  break;
-            case 5: printf("0");  break;
-            case 6: printf("0");  break;
-            case 7: printf("0");  break;
-            default : printf("\n\t\t===> PLEASE ENTER VALID CHOICE!!! <===\n\n");
-        }
-    }while(choicee != 0);
-
-
-
-
-
-    // printf("\t -------------------------------------------------------------------------------------------------------------------------------\n");
-    // printf("\t|\tN\t|\tID\t|\tMedicine Name\t|\tAmount\t|\tPrice Total\t|\tTransaction Date\t|\n");
-    // printf("\t ---------------------------------------------------------------------------------------------------------------------------------------\n");
-
+    // fichier = fopen("test.txt", "a+");
     // for(int i = 0; i < lentghArray; i++){
-        
-    //             printf("\t|\t%d\t|\t%d\t|\t%s    \t|\t%d\t|\t%.2f   DH\t|\t%s    \t\t|\n", i + 1, medicineArray[i].id, medicineArray[i].name, medicineArray[i].amount, medicineArray[i].price);
-            
-        
+    //     if (fichier != NULL)
+    //     {
+    //         fprintf(fichier, "%d\t%s    \t\t%d\t%.2f\n", medicineArray[i].id, medicineArray[i].name, medicineArray[i].amount, medicineArray[i].price); 
+    //     }
     // }
+    
+    // fclose(fichier); // On ferme le fichier qui a été ouvert
 
-    // printf("\t ---------------------------------------------------------------------------------------------------------------------------------------\n\n\n");
+    FILE* fichierr = NULL;
 
-    // char s[20];
-    // scanf("%s", s);
-    // toUpperCase(s);
-    // printf("%s", s);
-
-
-
-
-    // float moyen = totalProducts != 0 ? ((float)totalPrix / (float)totalProducts) : 0;
-    // printf("\t -------------------------------------------------------------------------------------------------------------------------------\n");
-    // printf("\t|\tSum Medicine Sold\t|\tMoyen Medicine Sold\t|\tMax Medicine Sold\t|\tMin Medicine Sold\t|\n");
-    // printf("\t -------------------------------------------------------------------------------------------------------------------------------\n");
-    // printf("\t|\t%.2f   DH\t\t|\t%.2f   DH\t\t|\t%.2f   DH\t\t|\t%.2f   DH\t\t|\n", 33, 33, 33, 22);
-    // printf("\t ---------------------------------------------------------------|\t%s        \t\t|\t%s\t\t|\n", "", "");
-    // printf("\t                                                                 ---------------------------------------------------------------\n");
-    // printf("------------ the sum price of all products : %d\n", 77);
-    // printf("------------ the moyen price of all products : %.2f\n", 44);
-    // printf("------------ the min of all products : %s\n", medicineArray[0].name);
-    // printf("------------ the max of all products : %s\n", medicineArray[0].name);
-
-    // printf("\t ----------\t----------\t----------\t----------\n");
-    // printf("\t|   name   |\t|    id    |\t|  amount  |\t|   prix   |\t|   date   |\n");
-    // printf("\t ----------\t----------\t----------\t----------\n");
-
-    // for(int i = 0; i < lentghArrayTransaction; i++){
-    //     printf("\t%s           \t%d           \t%d         \t%.2f           \t%s\n", transactionArray[i].nameProduct, transactionArray[i].idProduct, transactionArray[i].amountProductBuying, transactionArray[i].prixProductBuying, transactionArray[i].buyTime);
-    // }
-
-
-
-
-
-
-
-    // printf("\t -------------------------------------------------------\t\n");
-    // printf("\t|\t\t\tMENU\t\t\t\t|\n");
-    // printf("\t -------------------------------------------------------\t\n");
-    // printf("\t|\t{1}\t:\tShow Medicines\t\t\t|\n");
-    // printf("\t|\t{2}\t:\tAdd Medicines\t\t\t|\n");
-    // printf("\t|\t{3}\t:\tSearch for Medicine\t\t|\n");
-    // printf("\t|\t{4}\t:\tDelete Medicine\t\t\t|\n");
-    // printf("\t|\t{0}\t:\tExit\t\t\t\t|\n");
-    // printf("\t -------------------------------------------------------\t\n");
-}
+    fichierr = fopen("test.txt", "w+");
+    for(int i = 0; i < lentghArray; i++){
+        if (fichierr != NULL){
+            fprintf(fichierr, "%d\t%s    \t\t%d\t%.2f\n", medicineArray[i].id, medicineArray[i].name, medicineArray[i].amount, medicineArray[i].price); 
+        }
+    }
+    
+    fclose(fichierr); // On ferme le fichier qui a été ouvert   
+}   
